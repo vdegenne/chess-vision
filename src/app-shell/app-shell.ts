d9f1100bb79b380efbdbcfa39d9bfd2d23bc1898
vdegenne <vdegenne@gmail.com>
2024-09-07 17:39:36 +0200

    2024-09-07

---
+++ src/app-shell/app-shell.ts
@@ -0,0 +1,139 @@
+import {ReactiveController, state} from '@snar/lit';
+import {LitElement, html} from 'lit';
+import {ColorMode, withStyles} from 'lit-with-styles';
+import {customElement} from 'lit/decorators.js';
+import {classMap} from 'lit/directives/class-map.js';
+import {materialShellLoadingOff} from 'material-shell';
+import {saveToLocalStorage} from 'snar-save-to-local-storage';
+import {themeStore} from '../styles/styles.js';
+import styles from './app-shell.css?inline';
+import {SUCCESS_AUDIO, WRONG_AUDIO} from '../assets/assets.js';
+
+declare global {
+	interface Window {
+		app: AppShell;
+	}
+	interface HTMLElementTagNameMap {
+		'app-shell': AppShell;
+	}
+}
+
+const ROWS = [1, 2, 3, 4, 5, 6, 7, 8];
+const COLUMNS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+@saveToLocalStorage('chess-vision:state')
+class AppState extends ReactiveController {
+	@state() flipped = false;
+	@state() showCoords = false;
+	@state() coordQuestion = '';
+	@state() userAnswer = '';
+
+	pickNewCoordinate() {
+		this.userAnswer = '';
+		this.coordQuestion = `${COLUMNS[Math.floor(Math.random() * 8)]}${ROWS[Math.floor(Math.random() * 8)]}`;
+	}
+}
+const appstate = new AppState();
+
+@customElement('app-shell')
+@withStyles(styles)
+export class AppShell extends LitElement {
+	constructor() {
+		super();
+		appstate.bind(this);
+		appstate.updateComplete.then(async () => {
+			// console.log(appstate.coordQuestion);
+			if (!appstate.coordQuestion) {
+				appstate.pickNewCoordinate();
+			}
+		});
+	}
+
+	firstUpdated() {
+		materialShellLoadingOff.call(this);
+		themeStore.bind(this);
+	}
+
+	render() {
+		const rows = appstate.flipped ? ROWS.slice() : ROWS.slice().reverse();
+		const cols = appstate.flipped ? COLUMNS.slice().reverse() : COLUMNS.slice();
+
+		return html`
+			<div id="main-container">
+				<header>
+					<md-filled-button
+						?inert="${!appstate.userAnswer}"
+						@click=${() => {
+							appstate.pickNewCoordinate();
+						}}
+					>
+						<md-icon slot="icon">not_listed_location</md-icon>
+						${appstate.userAnswer
+							? 'New question'
+							: `Find ${appstate.coordQuestion}`}
+					</md-filled-button>
+					<div style="flex:1"></div>
+					<md-icon-button
+						@click=${() => (appstate.showCoords = !appstate.showCoords)}
+					>
+						${appstate.showCoords
+							? html`<md-icon>visibility</md-icon>`
+							: html`<md-icon>visibility_off</md-icon>`}
+					</md-icon-button>
+					<md-icon-button @click="${() => themeStore.toggleMode()}">
+						${themeStore.colorMode === ColorMode.DARK
+							? html`<md-icon>dark_mode</md-icon>`
+							: html`<md-icon>light_mode</md-icon>`}
+					</md-icon-button>
+					<md-icon-button
+						@click=${() => (appstate.flipped = !appstate.flipped)}
+					>
+						<md-icon>wifi_protected_setup</md-icon>
+					</md-icon-button>
+				</header>
+				<div id="board">
+					<md-elevation></md-elevation>
+					${rows.map((row, ri) => {
+						return cols.map((col, ci) => {
+							const coord = `${col}${row}`;
+							const isLight = (ri + ci) % 2 === 0;
+							const classes = classMap({
+								square: true,
+								light: isLight,
+								dark: !isLight,
+							});
+							return html`<div
+								class="${classes}"
+								@click=${() => {
+									if (!appstate.userAnswer) {
+										appstate.userAnswer = coord;
+										if (appstate.userAnswer === appstate.coordQuestion) {
+											SUCCESS_AUDIO.play();
+										} else {
+											WRONG_AUDIO.play();
+										}
+									}
+								}}
+							>
+								<div class="coordinate">
+									${appstate.showCoords ? coord : null}
+									${appstate.userAnswer
+										? html`
+												${coord === appstate.userAnswer &&
+												appstate.userAnswer !== appstate.coordQuestion
+													? html`❌`
+													: null}
+												${coord === appstate.coordQuestion ? html`✅` : null}
+											`
+										: null}
+								</div>
+							</div>`;
+						});
+					})}
+				</div>
+			</div>
+		`;
+	}
+}
+
+export const app = (window.app = new AppShell());
---
+++ src/styles/styles.ts
@@ -0,0 +1,54 @@
+import {ReactiveController} from '@snar/lit';
+import {PropertyValues} from 'lit';
+import {ColorMode, ThemeManager} from 'lit-with-styles';
+import {state} from 'snar';
+import {saveToLocalStorage} from 'snar-save-to-local-storage';
+
+@saveToLocalStorage('sfc:theme')
+class ThemeStore extends ReactiveController {
+	@state() colorMode = ColorMode.SYSTEM;
+	/**
+	 * When changing the following default value, we also have
+	 * to make sure to provide the tokens on start,
+	 * and also 'theme-color' meta tag in html header.
+	 * Material default theme seed is '#6750A4'
+	 */
+	@state() themeColor = '#6750A4';
+
+	async updated(changed: PropertyValues) {
+		if (changed.has('colorMode')) {
+			ThemeManager.mode = this.colorMode;
+		}
+		const {themeFromSourceColor, applyTheme} = await import(
+			'@vdegenne/material-color-helpers'
+		);
+		const theme = themeFromSourceColor(
+			this.themeColor,
+			ThemeManager.appliedColorScheme === 'dark',
+			'vibrant',
+			0,
+		);
+		applyTheme(document, theme!);
+	}
+
+	toggleMode() {
+		const currentScheme = ThemeManager.appliedColorScheme!;
+		const oppositeMode =
+			currentScheme === 'dark' ? ColorMode.LIGHT : ColorMode.DARK;
+		this.colorMode = oppositeMode;
+	}
+}
+
+export const themeStore = (window.themeStore = new ThemeStore());
+
+window
+	.matchMedia('(prefers-color-scheme: dark)')
+	.addEventListener('change', () => themeStore.requestUpdate());
+
+declare global {
+	interface Window {
+		themeStore: ThemeStore;
+	}
+}
+
+ThemeManager.init();
