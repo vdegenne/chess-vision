199f1b5c20ed1d73de5f813393fd3d9d51f13847
vdegenne <vdegenne@gmail.com>
2024-09-08 15:05:59 +0200

    2024-09-08

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
 			variant: 'rounded',
 			include: [
 				'src/**/*.ts',
-				// 'node_modules/@vdegenne/material-color-helpers/lib/elements/**/*.js',
+				'node_modules/@vdegenne/material-color-helpers/lib/elements/**/*.js',
 			],
 		}),
 
